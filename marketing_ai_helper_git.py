60d261216a977c8e6acff36ea2cd421edf1853e2
KuperOK <kupershtein.lm@gmail.com>
2024-09-20 18:43:42 +0300

    v01

---
+++ marketing_ai_helper_git.py
@@ -0,0 +1,232 @@
+import streamlit as st
+import pandas as pd
+import re
+import openai
+import os
+from langchain_community.document_loaders import WebBaseLoader as WBL
+from langchain.prompts import ChatPromptTemplate
+from prompts import * #prompt_for_title
+from langchain_openai import ChatOpenAI
+import io
+from datetime import datetime
+
+en = '?language=en_GB&th=1'
+ge = '?th=1&language=de_DE&currency=EUR'
+
+def get_title_and_text_from_amazon(url, lang):
+    url = url + lang
+    loader = WBL(web_path=url)
+    docs = loader.load()
+    long_name = docs[0].metadata['title']
+    other_text = docs[0].page_content
+    info_start_idx = other_text.find('Info zu diesem Artikel') if 'de_' in lang else other_text.find('About this item')      
+    info_end_idx = other_text.find('Weitere Produktdetails') if 'de_' in lang else other_text.find('See more product details')
+    description = ''
+    if info_start_idx > 0:
+        description = other_text[info_start_idx:info_end_idx]
+    comments_start_idx = other_text.find('Spitzenrezensionen') if 'de_' in lang else other_text.find('Top reviews')
+    comments_end_idx = other_text.rfind('Weitere Rezensionen ansehen') if 'de_' in lang else other_text.find('See more reviews')
+    comments = ''
+    if comments_start_idx > 0:
+        comments = other_text[comments_start_idx:comments_end_idx]    
+        comments = re.sub(r'\n{2,}', '\n', comments)
+        comments = re.sub(r'Report\s*Translate review to English', '', comments)
+    product_text = {}
+    product_text['long name'] = long_name
+    product_text['description'] = description
+    product_text['comments'] = comments
+    return product_text
+
+
+def clean_key(key):
+    """
+    Cleans the input string (key) by removing unwanted characters.
+
+    The function performs the following steps:
+    1. Removes any leading numbers and dots from the beginning of the string.
+    2. Removes all characters that are not letters, digits, or spaces (including special characters).
+    3. Removes all non-ASCII characters (such as Unicode symbols and ideograms).
+    4. Trims any leading and trailing whitespace from the string.
+
+    Args:
+        key (str): The input string representing a key.
+
+    Returns:
+        str: The cleaned string without numbers, special characters, or non-ASCII symbols.
+    """
+    key = re.sub(r'^[\d.]+', '', key)  # Removes leading numbers and dots
+    key = re.sub(r'[^\w\s]', '', key)  # Removes all non-alphanumeric characters and spaces
+    key = re.sub(r'[^\x00-\x7F]+', '', key)  # Removes all non-ASCII characters
+    key = key.strip()  # Trims leading and trailing spaces
+    return key
+
+def get_technical_data(file_path: str) -> dict:
+    """
+    Extracts and cleans technical data from an Excel file.
+
+    This function reads technical information from a specific Excel sheet, processes the data by:
+    1. Loading the Excel sheet named '2. Technical Info' and skipping the first 4 rows.
+    2. Selecting only the specified columns and dropping rows with missing values.
+    3. Filtering out rows where the 'value' column contains the string 'Choose from dropdown'.
+    4. Converting the remaining data into a dictionary format.
+    5. Cleaning the keys of the dictionary using the `clean_key` function to remove unwanted characters and symbols.
+
+    Args:
+        file_path (str): The path to the Excel file containing the technical data.
+
+    Returns:
+        dict: A dictionary where keys represent technical feature names and values represent the corresponding data.
+    """
+    temp = pd.ExcelFile(file_path)
+    sheet_name = None
+    for name in temp.sheet_names:
+        if 'Technical Info' in name:
+            sheet_name = name
+            break
+    else:
+        raise ValueError('There isn\'t Technical Info in excel-file')
+        
+    # print(sheet_name)
+    df = pd.read_excel(file_path, header=None, skiprows=4, sheet_name=sheet_name, usecols=[1,2])
+    df = df.dropna(axis=0)
+    df.columns=['name', 'value']
+    df = df[~(df.value == 'Choose from dropdown')]
+    df['name'] = df.name.apply(clean_key)
+    features = df.to_dict(orient='tight', index=False)
+    features = dict(features['data'])
+    # features = {clean_key(key): value for key, value in features.items()}
+    return features, df
+
+
+
+st.title(':blue[Marketing AI Helper]')
+
+# 1. Data Upload Section
+st.header(":orange[Upload Analogue Product Data]")
+
+url = st.text_input("Enter a hyperlink of similar product:")
+show_product_params = st.checkbox('Show analogue product params', value=False)
+
+if 'product' not in st.session_state:
+    st.session_state.product = None
+if url and st.button('Retrieving'):
+    with st.spinner(text="Retrieving analogue product data is in progress ..."):
+        product = get_title_and_text_from_amazon(url, en)
+        st.session_state.product = product
+    # st.markdown("**Analogue long name:**")
+    if show_product_params:
+        prod_long_name = st.text_area("**Analogue long name:**" ,value=product['long name'])            
+        product_decription = st.text_area("Analogue description:", value=product['description'])            
+        product_comments = st.text_area("Analogue comments:", value=product['comments'])
+   
+
+uploaded_file = st.file_uploader("Upload Excel file:", type=["xlsx", "xls"])
+show_product_tinfo = st.checkbox('Show product tech info', value=False)
+if uploaded_file is not None:
+    tech_info, df = get_technical_data(uploaded_file)
+    df.columns = ['Feature', 'Value']
+    if show_product_tinfo:
+        st.subheader("Tech info from Excel file:")
+        # st.dataframe(pd.DataFrame([tech_info.keys(), tech_info.values()], columns=['Feature', 'Value']))
+        st.dataframe(df)
+
+
+# 2. Product Description Generation Section
+st.header(":green[Generate New Product Description]")
+
+# Block 1: Long Name Generation
+st.subheader("Generate Long Name")
+
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+llm_model = "gpt-4o-mini-2024-07-18" # chatgpt-4o-latest
+chat = ChatOpenAI(model=llm_model, temperature=0.5)
+
+prompt_long_name = st.text_area("Enter prompt for long name:", value=prompt_for_title, height=350)
+prompt_template_long_name = ChatPromptTemplate.from_template(prompt_long_name)
+# formatted_prompt = prompt_for_title.format(tech_info=tech_info, prod_long_name=prod_long_name)
+if  st.session_state.product and tech_info:
+    prompt_template_long_name = prompt_template_long_name.format(tech_info=tech_info, prod_long_name=st.session_state.product['long name'])
+
+
+if st.button("Generate Long Name"):
+    st.session_state.response_title = chat.invoke(prompt_template_long_name).content
+    st.text_area("Generated Long Name:", st.session_state.response_title, height=275)
+
+# Block 2: Benefits Generation
+st.subheader("Generate Product Benefits")
+prompt_benefits = st.text_area("Enter prompt for benefits:", value=prompt_for_benefits, height=500)
+if  st.session_state.product:
+    prompt_template_benefits = ChatPromptTemplate\
+        .from_template(prompt_benefits)\
+        .format(
+            tech_info=tech_info,
+            product_decription = st.session_state.product['description']
+            )
+if st.button("Generate Benefits"):
+    st.session_state.response_benefits = chat.invoke(prompt_template_benefits).content
+    
+    with st.container(border=True, height=200):
+        st.write("Product Benefits: ")
+        st.markdown(st.session_state.response_benefits)
+
+# Block 3: USP Generation
+st.subheader("Generate USP")
+prompt_usp = st.text_area("Enter prompt for USP:", value=prompt_for_USP, height=700)
+if  st.session_state.product:
+    prompt_template_USP = ChatPromptTemplate\
+        .from_template(prompt_usp)\
+        .format(
+            tech_info=tech_info,
+            product_decription = st.session_state.product['description'],
+            product_comments = st.session_state.product['comments']        
+        )
+if st.button("Generate USP"):
+    # # Add code to send a request to the ChatGPT API and get the result
+    # generated_usp = "Generated USP" # Replace with actual ChatGPT result
+    # st.text_area("Generated USP:", generated_usp)
+    st.session_state.response_USP = chat.invoke(prompt_template_USP).content
+    with st.container(border=True, height=500):
+        st.write("Product unique selling points (USP): ")
+        st.markdown(st.session_state.response_USP)
+
+# Block 4: Marketing Text Generation
+st.subheader("Generate Marketing Text")
+prompt_marketing_text = st.text_area("Enter prompt for advertisin text:", value=prompt_for_ad_text, height=700)
+if  st.session_state.product:
+    prompt_template_text = ChatPromptTemplate\
+        .from_template(prompt_marketing_text)\
+        .format(
+            tech_info=tech_info,
+            product_decription = st.session_state.product['description'],
+            product_comments = st.session_state.product['comments'] 
+        )
+if st.button("Generate Marketing Text"):
+    st.session_state.response_text = chat.invoke(prompt_template_text).content
+    with st.container(border=True, height=750):
+        st.write("Product adverising text: ")
+        st.markdown(st.session_state.response_text)
+
+
+# 3. Saving and Clearing Section
+st.header(":red[Save and Clear]")
+
+if st.button("Save Results"):
+    hs_code = tech_info.get('HS Code', 'xxxx')
+    filename = str(hs_code) + '_marketing_info_' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.txt'
+    all_togather = f"{st.session_state.response_title}\n{st.session_state.response_benefits}\n\n{st.session_state.response_USP}\n\n{st.session_state.response_text}"
+    buffer = io.BytesIO()
+    buffer.write(all_togather.encode("utf-8"))
+    buffer.seek(0)
+    if st.download_button(
+                label="Download File",
+                data=buffer,
+                file_name=filename,
+                mime="text/plain"
+            ):
+        st.success("Results saved!")
+
+# if st.button("Clear"):
+#     # Add code to clear all input fields and results
+#     st.experimental_rerun()
---
+++ prompts.py
@@ -0,0 +1,102 @@
+prompt_for_title = """You are an expert at creating detailed product titles for an online store.
+I need three different long product titles for an item based on its technical specifications provided below.
+The titles should be optimized for search engines, customer-friendly,
+and include the most important technical details in a concise but informative way.
+Here are the technical specifications for the product:\n{tech_info}
+
+Example: Here's an example of a long product title for reference: {prod_long_name}
+
+Please generate three product title variations for the given technical information using this format and structure.
+Don't use any info from analog and don't use Device Name displayed in Network.
+Show only your names without any comments."""
+
+
+prompt_for_benefits = """You are an expert in marketing and product descriptions. 
+I need you to create three customer-oriented benefits for an item based on its technical specifications provided below. 
+The benefits should be simple, clear, and highlight how the product can improve the user's experience.
+
+Here are the technical specifications for the product:
+{tech_info}
+
+Additionally, you may refer to the description of a similar product provided below to inspire the benefits:
+{product_decription}
+
+Here is an example for another item:
+- "Promotes peaceful sleep with soothing light effects"
+- "Easy to use – perfect atmosphere for children’s rooms"
+- "Creative gift: transforms any room into a starry sky"
+
+Please generate three benefit descriptions for the given product not longer 80 symbols, focusing on how it solves user problems or adds value. Show only your responses without any comments.
+"""
+
+prompt_for_USP = """
+You are an expert in marketing and product descriptions. 
+I need you to create six customer-oriented Unique Selling Points for an item based on its technical specifications provided below. 
+The Unique Selling Points should be simple, clear, and highlight how the product can improve the user's experience.
+
+Here are the technical specifications for the product:
+{tech_info}
+
+Additionally, you may refer to the description of a similar product provided below to inspire the  Unique Selling Points:
+{product_decription}
+
+Additionally, you may refer to the comments of a similar product provided below to inspire the  Unique Selling Points:
+{product_comments}
+
+Here is an example for another item:
+*Version 1:
+-Unique 3D Northern Lights: Create a magical atmosphere.
+-18 lighting modes: The right lighting for every mood.
+-Energy-saving: Low power consumption with DC5V.
+-Easy to use: Easy to switch between different modes.
+-Compact design: Fits perfectly in any child's room.
+-Robust and safe: Made from durable materials.
+*Version 2:
+-Realistic star projection: Creates a magical environment.
+-Various light settings: Adapts to every mood.
+-Safe and durable: Suitable for everyday use.
+-Easy installation: Ready to use quickly and very easy to use.
+-Perfect as a gift: Ideal for children's birthdays and festive occasions.
+-Silent operation: No annoying noises during use.
+*Version 3:
+-Creative light show: Delights children with changing colors.
+-Environmentally friendly: Low energy consumption for longer use.
+-Child-friendly design: Safe and easy to use.
+-Long-lasting LED technology: Lasts longer and offers consistent quality.
+-Mobile use: Can be used anywhere thanks to its compact format.
+-Attractive design: Fits into any interior design in a child's room.
+
+Please generate three different variants by six USP in eschbenefit descriptions for the given product, focusing on uniqueness, Customer Benefits and Competitive advantages.
+Length of each USP no more than 85 characters
+Show only your responses without any comments.
+"""
+
+prompt_for_ad_text = """
+Create an advertising text of up to 3000 characters for a product with the following technical specifications: 
+{tech_info}.
+You may use as example of advertising text the description of a similar product provided below: 
+{product_decription}.
+Strictly prohibited use technical info from product description for advertising texts
+
+Additionally, you can utilize in general useful insights from customer reviews, but do not directly reference them in the text.
+Сustomer reviews:
+{product_comments}
+
+Strictly prohibited use technical info from customer review for advertising texts
+
+Begin with a short, general introduction (maximum 2-3 sentences). 
+Then, write between 3 to 6 meaningful paragraphs, each starting with **6 promotional words** that describe the feature highlighted in that paragraph. 
+These 6 words should be bolded, followed by the rest of the text after a colon.
+
+List the technical aspects of the product in a bullet-point format, using as many points as needed. 
+Please emphasize the most important information by making it bold. Befor technical info paste `Technical details: ` 
+
+Please create 3 variations:
+1. Highly technical description
+2. Less technical description
+3. More conversational, user-friendly tone
+
+Take into account - In advertising text you can add only info from technical specifications and dont use it from similar product. It is very importantly/
+Strictly prohibited using technical info of similar product for advertising texts only from {tech_info} 
+Show only your responses without any comments.
+"""
